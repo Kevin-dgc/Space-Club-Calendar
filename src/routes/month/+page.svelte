bd56780dcda754c126c7d2047f5eb52d9d034cee
makardric <rccam@csu.fullerton.edu>
2025-04-12 20:01:27 -0700

    added functions in day

--- src/routes/month/+page.svelte
+++ src/routes/month/+page.svelte
@@ -3,7 +3,32 @@
 
 
 <script>
+    import { page } from '$app/stores';
 
+    $: monthNumber = parseInt($page.params.month);
+    $: year = new Date().getFullYear();
+
+    $: monthName = new Date(year, monthNumber - 1, 1).toLocaleString('default', { month : 'long'});
+    $: daysInMonth = new Date(year, monthNumber, 0).getDate();
+    $: firstDayOfWeek = new Date(year, monthNumber - 1, 1).getDay(); 
+
+    function generateCalendarDays() {
+        const days =  [];
+
+        for (let i = 0; i < firstDayOfWeek; i++) {
+            days.push({ day: null, isCurrentMonth: false});
+        }
+
+        for (let day = 1; day <= daysInMonth; day++) {
+            days.push({
+                day,
+                isCurrentMonth: true,
+                date: new Date(year, monthNumber - 1, day)
+            });
+        }
+
+        return day;
+    }
 </script>
 
 <style>
