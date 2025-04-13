9f2235ed88d934307658dc8dd22008130ef4a5ee
makardric <rccam@csu.fullerton.edu>
2025-04-12 22:17:34 -0700

    added real images to the month buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <p>Visit <a href="/month">svelte.dev/docs/kit</a> to read the documentation</p>
 <script>
 const months = [   
-    { name: "Jan", num: 1},   
-    { name: "Feb", num: 2},   
-    { name: "Mar", num: 3},   
-    { name: "Apr", num: 4},   
-    { name: "May", num: 5},   
-    { name: "Jun", num: 6},   
-    { name: "Jul", num: 7},   
-    { name: "Aug", num: 8},   
-    { name: "Sep", num: 9},   
-    { name: "Oct", num: 10},  
-    { name: "Nov", num: 11},   
-    { name: "Dec", num: 12} ]
+    { name: "Jan", num: 1, img: "1.png"},   
+    { name: "Feb", num: 2, img: "2.png"},   
+    { name: "Mar", num: 3, img: "3.png"},   
+    { name: "Apr", num: 4, img: "4.png"},   
+    { name: "May", num: 5, img: "5.png"},   
+    { name: "Jun", num: 6, img: "6.png"},   
+    { name: "Jul", num: 7, img: "7.png"},   
+    { name: "Aug", num: 8, img: "8.png"},   
+    { name: "Sep", num: 9, img: "9.png"},   
+    { name: "Oct", num: 10, img: "10.png"},  
+    { name: "Nov", num: 11, img: "11.png"},   
+    { name: "Dec", num: 12, img: "12.png"} ]
 
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth() + 1;
@@ -22,7 +22,7 @@ const months = [
 <div class="months-grid">
     {#each months as month}
       <a href="/month/{month.num}" class="month-card">
-        <h2> <img alt="The icon" src={"/favicon.png"} />  </h2>
+        <h2> <img alt="The icon" src={month.img} />  </h2>
       </a>
     {/each}
   </div>
