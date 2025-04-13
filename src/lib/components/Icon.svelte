6a0e2cd0ef3f80cf8f85cf8043fa3d86fae36531
Kevin-dgc <kevingeierconney@gmail.com>
2025-04-12 18:18:08 -0700

    2nd commit

--- src/lib/components/Icon.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  interface Props {
-    icon: string;
-  }
-
-  let { icon }: Props = $props();
-</script>
-
-<span class="material-icons">{icon}</span>
--- src/routes/+error.svelte
+++
@@ -1,9 +0,0 @@
-<p class="err centered">Error ( ͡° ͜ʖ ͡°)</p>
-
-<style>
-  .err {
-    color: var(--pico-del-color);
-    font-size: x-large;
-    font-weight: 500;
-  }
-</style>
