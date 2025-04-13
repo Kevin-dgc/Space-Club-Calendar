32fa96e67c201c84d038008ace4f8e28ba922858
makardric <rccam@csu.fullerton.edu>
2025-04-12 19:11:03 -0700

    finished month array

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
 
 <script>
-const month = [   { name: "Jan", num: 1}   { name: "Feb", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1}   { name: "Jan", num: 1} ]
+const month = [   { name: "Jan", num: 1},   { name: "Feb", num: 2},   { name: "Mar", num: 3},   { name: "Apr", num: 4},   { name: "May", num: 5},   { name: "Jun", num: 6},   { name: "Jul", num: 7},   { name: "Aug", num: 8},   { name: "Sep", num: 9},   { name: "Oct", num: 10},   { name: "Nov", num: 11},   { name: "Dec", num: 12} ]
 </script>
 
 <style>
