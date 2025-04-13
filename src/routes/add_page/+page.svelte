f93c121b0d9aefb31d36f2f5357fd6d57106c736
makardric <rccam@csu.fullerton.edu>
2025-04-13 13:51:26 -0700

    implemented the calendar input and time list input successfully

--- src/routes/add_page/+page.svelte
+++ src/routes/add_page/+page.svelte
@@ -19,7 +19,9 @@ function handleAdd() {
         // tell user to input all data
     }
     else{
-        addEvent(org, name, date, time, link, description);
+      let [year, month, day] = date.split('-'); // ["2025", "01", "01"]
+      let cleanedDate = `${Number(month)}/${Number(day)}/${year}`; // "1/1/2025"
+        addEvent(org, name, cleanedDate, time, link, description);
         org = '';
         name = '';
         date = '';
@@ -70,8 +72,23 @@ function handleAdd() {
     <!-- Each input is bound to a variable -->
     <input type="text" bind:value={org} placeholder="Enter org name" />
     <input type="text" bind:value={name} placeholder="Enter name of event" />
-    <input type="text" bind:value={date} placeholder="Enter date of event" />
-    <input type="text" bind:value={time} placeholder="Enter time of event" />
+    <input id="calendar" type="date" bind:value={date} />
+    <input list="timeList" bind:value={time} name = "time" />
+    <datalist id = "timeList">
+      <option value = "8:00 AM"></option>
+      <option value = "9:00 AM"></option>
+      <option value = "10:00 AM"></option>
+      <option value = "11:00 AM"></option>
+      <option value = "12:00 PM"></option>
+      <option value = "1:00 PM"></option>
+      <option value = "2:00 PM"></option>
+      <option value = "3:00 PM"></option>
+      <option value = "4:00 PM"></option>
+      <option value = "5:00 PM"></option>
+      <option value = "6:00 PM"></option>
+      <option value = "7:00 PM"></option>
+      <option value = "8:00 PM"></option>
+    </datalist>
     <input type="text" bind:value={link} placeholder="Enter link to social" />
     <input type="text" bind:value={description} placeholder="Enter description of event" />
   
